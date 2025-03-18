42592d100fc8929a507c57541a643b03a6dbf900
ingxrd <ingridmunhoz86@gmail.com>
2025-03-18 10:04:36 -0300

    Adicionando funcoes js

---
+++ script.js
@@ -0,0 +1,50 @@
+document.getElementById("calculate").addEventListener("click", 
+    function() {
+        //Essa parte cria constantes para salvar os inputs do usuário.
+
+        const numero1 = document.getElementById("number1").value;
+        const numero2 = document.getElementById("number2").value;
+        const operator = document.getElementById("operator").value;
+
+        // Agora que tenho os elementos do DOM na minha funcao, consigo criar, usando switch ou if-else, enfim, 
+        // a forma de calcular. Como estou falando de varios casos, vou optar pelo uso do SWITCH
+
+        let result
+
+        switch(operator){
+            case '+':  
+                result = number1 + number2;
+                    break;
+            case '-':
+                result = number1 - number2;
+                    break;
+            case '*':
+                result = number1 * number2;
+                    break;
+            case '/': 
+                result = number1 / number2;
+                    break; 
+            
+            default:
+                result = "Operaçao inválida"
+        }
+    
+        document.getElementById('result').textContent = result;
+    }
+
+)
+
+
+
+
+/*
+    Existem 5 elementos importantes a serem manipulados no DOM:
+        1. Number1 => primeiro numero a ser inserido.
+        2. Number2 => segundo numero a ser inserido.
+        3. Operator => operador matemático
+        4. Result => resultado.
+        5. Calculate => calcula 
+
+        O principal elemento é o calculate. Essa funcao receberá os numeros e operadores, bem como o botao de calcular.
+*/
+
